2ad521089efbfd037d65fa7b15d59a442c2d5698
HieuNguyen <114110346+hieunguyendev2206@users.noreply.github.com>
2023-06-26 21:26:53 +0700

    Update main.js

--- Shop_Thoi_Trang_Nam_Template/js/main.js
+++ Shop_Thoi_Trang_Nam_Template/js/main.js
@@ -155,24 +155,6 @@
     });
 
 
-
-    var today = new Date();
-    var dd = String(today.getDat()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0');
-    var yyyy = today.getFullYear();
-
-    if ( mm = 12) {
-        mm = '01';
-        yyyy = yyyy + 1;
-    }
-    else {
-        mm = parseInt(mm) + 1;
-        mm = String(mm).padStart(2, '0');
-    }
-    var timerdate = mm + '/' + dd + '/' + yyyy
-
-
-
     /*-------------------
 		Quantity change
 	--------------------- */
